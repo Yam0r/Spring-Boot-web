d4ee3b4a8e86982b68e0b86f8cd3f425938ecdea
Yarolsav <kiritok712@gmail.com>
2024-06-27 22:00:00 +0200

    web-intro

---
+++ src/main/java/mate/academy/onlinebookstore/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,63 @@
+package mate.academy.onlinebookstore.repository.impl;
+
+import java.util.List;
+import java.util.Optional;
+import mate.academy.onlinebookstore.exception.DataProcessingException;
+import mate.academy.onlinebookstore.model.Book;
+import mate.academy.onlinebookstore.repository.BookRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert book into DB: " + book, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            Book book = session.find(Book.class, id);
+            return Optional.ofNullable(book);
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find books from DB with id = " + id, e);
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("SELECT b FROM Book b", Book.class).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get all books from DB", e);
+        }
+    }
+}
---
+++ src/test/java/mate/academy/onlinebookstore/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,12 @@
+package mate.academy.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
